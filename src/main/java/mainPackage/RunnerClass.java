f5551c31882a249d5753a5ae9d0dc294ba21b935
gopivasala01 <gopi.v@beetlerim.com>
2023-01-03 07:20:35 -0500

    New

---
+++ src/main/java/mainPackage/RunnerClass.java
@@ -0,0 +1,33 @@
+package mainPackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class RunnerClass implements Runnable
+{
+
+   public static void main(String[] args) 
+   {
+	   for(int i=0;i<5;i++)
+	   {
+		   RunnerClass r = new RunnerClass();
+		   Thread t = new Thread(r);
+            t.start();
+	   }
+	   
+   }
+   
+
+public void run() 
+{
+	   WebDriverManager.chromedriver().setup();
+	   ChromeDriver driver = new ChromeDriver();
+	   driver.get("https://app.propertyware.com/pw/login.jsp");
+	   driver.findElement(By.name("email")).sendKeys("mds0418@gmail.com");
+	   driver.findElement(By.name("password")).sendKeys("HomeRiver1#");
+	   driver.findElement(By.xpath("//*[@value='Sign Me In']")).click();	
+}
+	
+}
