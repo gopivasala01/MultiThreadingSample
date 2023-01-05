17821056c3b2ffd9d9479f8139bcec0d316ea9b2
gopivasala01 <49265100+gopivasala01@users.noreply.github.com>
2023-01-05 14:08:31 +0530

    Update RunnerClass.java

--- src/main/java/mainPackage/RunnerClass.java
+++ src/main/java/mainPackage/RunnerClass.java
@@ -2,6 +2,7 @@ package mainPackage;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
