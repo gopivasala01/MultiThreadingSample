a9cb6f324bd31a93c5fc9e5630cb98c351797c60
gopivasala01 <49265100+gopivasala01@users.noreply.github.com>
2023-01-05 16:19:24 +0530

    Update RunnerClass.java

--- src/main/java/mainPackage/RunnerClass.java
+++ src/main/java/mainPackage/RunnerClass.java
@@ -29,14 +29,14 @@ public void run()
 	   ChromeDriver driver = new ChromeDriver(options);
 	 // options.setExperimentalOption("prefs", chromePrefs);
 	
-	options.add_argument("--headless")
-options.add_argument("window-size=1400,1500")
-options.add_argument("--disable-gpu")
-options.add_argument("--no-sandbox")
-options.add_argument("start-maximized")
-options.add_argument("enable-automation")
-options.add_argument("--disable-infobars")
-options.add_argument("--disable-dev-shm-usage")
+	options.add_argument("--headless");
+options.add_argument("window-size=1400,1500");
+options.add_argument("--disable-gpu");
+options.add_argument("--no-sandbox");
+options.add_argument("start-maximized");
+options.add_argument("enable-automation");
+options.add_argument("--disable-infobars");
+options.add_argument("--disable-dev-shm-usage");
 
 	
 	   
