df5106fa338e250d839abceeece40653ce8ff5fb
gopivasala01 <49265100+gopivasala01@users.noreply.github.com>
2023-01-06 10:39:23 +0530

    Update RunnerClass.java

--- src/main/java/mainPackage/RunnerClass.java
+++ src/main/java/mainPackage/RunnerClass.java
@@ -26,24 +26,23 @@ public void run()
 	   WebDriverManager.chromedriver().setup();
 	   
 	   ChromeOptions options = new ChromeOptions();
-	     options.add_argument("--headless");
-	options.add_argument("window-size=1400,1500");
-	options.add_argument("--disable-gpu");
-	options.add_argument("--no-sandbox");
-	options.add_argument("start-maximized");
-	options.add_argument("enable-automation");
-	options.add_argument("--disable-infobars");
-	options.add_argument("--disable-dev-shm-usage");
-	   ChromeDriver driver = new ChromeDriver(options);
+	   
 	 // options.setExperimentalOption("prefs", chromePrefs);
 	
-	
+	options.addArguments("--headless");
+	options.addArguments("window-size=1400,1500");
+	options.addArguments("--disable-gpu");
+	options.addArguments("--no-sandbox");
+	options.addArguments("start-maximized");
+	options.addArguments("enable-automation");
+	options.addArguments("--disable-infobars");
+	options.addArguments("--disable-dev-shm-usage");
 
-	   
+	ChromeDriver driver = new ChromeDriver(options);	   
 	   driver.get("https://app.propertyware.com/pw/login.jsp");
 	   driver.findElement(By.name("email")).sendKeys("mds0418@gmail.com");
 	   driver.findElement(By.name("password")).sendKeys("HomeRiver1#");
-	   driver.findElement(By.xpath("//*[@value='Sign Me In']")).click();	
+	   driver.findElement(By.xpath("//*[@value='Sign Me In']")).click();
 }
 	
 }
