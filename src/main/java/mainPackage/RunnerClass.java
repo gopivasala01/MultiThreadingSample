0a956831c19a927991176b871a0266dc8a8c4a87
gopivasala01 <49265100+gopivasala01@users.noreply.github.com>
2023-01-06 10:36:50 +0530

    Update RunnerClass.java

--- src/main/java/mainPackage/RunnerClass.java
+++ src/main/java/mainPackage/RunnerClass.java
@@ -26,17 +26,18 @@ public void run()
 	   WebDriverManager.chromedriver().setup();
 	   
 	   ChromeOptions options = new ChromeOptions();
+	     options.add_argument("--headless");
+	options.add_argument("window-size=1400,1500");
+	options.add_argument("--disable-gpu");
+	options.add_argument("--no-sandbox");
+	options.add_argument("start-maximized");
+	options.add_argument("enable-automation");
+	options.add_argument("--disable-infobars");
+	options.add_argument("--disable-dev-shm-usage");
 	   ChromeDriver driver = new ChromeDriver(options);
 	 // options.setExperimentalOption("prefs", chromePrefs);
 	
-	options.add_argument("--headless");
-options.add_argument("window-size=1400,1500");
-options.add_argument("--disable-gpu");
-options.add_argument("--no-sandbox");
-options.add_argument("start-maximized");
-options.add_argument("enable-automation");
-options.add_argument("--disable-infobars");
-options.add_argument("--disable-dev-shm-usage");
+	
 
 	   
 	   driver.get("https://app.propertyware.com/pw/login.jsp");
