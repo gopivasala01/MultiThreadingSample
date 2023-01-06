bc32345ba35e286c6f4fc53829e42e48a4fa4142
gopivasala01 <49265100+gopivasala01@users.noreply.github.com>
2023-01-06 10:37:14 +0530

    Update RunnerClass.java

--- src/main/java/mainPackage/RunnerClass.java
+++ src/main/java/mainPackage/RunnerClass.java
@@ -11,7 +11,7 @@ public class RunnerClass implements Runnable
 
    public static void main(String[] args) 
    {
-	   for(int i=0;i<5;i++)
+	   for(int i=0;i<2;i++)
 	   {
 		   RunnerClass r = new RunnerClass();
 		   Thread t = new Thread(r);
