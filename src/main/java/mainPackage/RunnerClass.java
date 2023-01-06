85a286b50fcf43bf2a06a69fc0e2e120aa5af3c3
gopivasala01 <49265100+gopivasala01@users.noreply.github.com>
2023-01-06 12:07:47 +0530

    Update RunnerClass.java

--- src/main/java/mainPackage/RunnerClass.java
+++ src/main/java/mainPackage/RunnerClass.java
@@ -16,6 +16,7 @@ public class RunnerClass implements Runnable
 		   RunnerClass r = new RunnerClass();
 		   Thread t = new Thread(r);
             t.start();
+	    Thread.sleep(5000);
 	   }
 	   
    }
